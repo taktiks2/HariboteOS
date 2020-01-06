d8abfff7774f9215c40f5c4702d26c0ec2db5c07
unknown <deathproof.lee@gmail.com>
2020-01-05 19:29:53 -0500

    Done day7

--- day7/int.c
+++ day7/int.c
@@ -16,7 +16,7 @@ void init_pic(void)
 
 	io_out8(PIC1_ICW1, 0x11  ); // Edge trigger mode           
 	io_out8(PIC1_ICW2, 0x28  ); // IRQ8-15 is received by INT28-2f
-	io_out8(PIC1_ICW3, 2     ); // PIC1 is connected at IRW2   
+	io_out8(PIC1_ICW3, 2     ); // PIC1 is connected at IRQ2   
 	io_out8(PIC1_ICW4, 0x01  ); // Non-buffer mode             
 
 	io_out8(PIC0_IMR,  0xfb  ); // Do not accept all interrupts
@@ -27,30 +27,29 @@ void init_pic(void)
 
 #define PORT_KEYDAT		0x0060
 
-struct KEYBUF keybuf;
+struct FIFO8 keyfifo;
 
 void inthandler21(int *esp)
 // Interrupt from PS/2 keyboard
 {
 	unsigned char data;
-	io_out8(PIC0_OCW2, 0x61); // Notify the reception completion of IRQ-01
+	io_out8(PIC0_OCW2, 0x61); // Notify PIC of IRQ-01 reception completion
 	data = io_in8(PORT_KEYDAT);
-	if (keybuf.flag == 0) {
-		keybuf.data = data;
-		keybuf.flag = 1;
-	}
+	fifo8_put(&keyfifo, data);
 	return;
 }
+
+struct FIFO8 mousefifo;
 
 void inthandler2c(int *esp)
 // Interrupt from PS/2 mouse
 {
-	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
-	boxfill8(binfo->vram, binfo->scrnx, COL8_000000, 0, 0, 32 * 8 - 1, 15);
-	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, "INT 2C (IRQ-12) : PS/2 mouse");
-	for (;;) {
-		io_hlt();
-	}
+	unsigned char data;
+	io_out8(PIC1_OCW2, 0x64); // Notify PIC1 of IRQ-12 reception completion
+	io_out8(PIC0_OCW2, 0x62); // Notify PIC0 of IRQ-02 reception completion
+	data = io_in8(PORT_KEYDAT);
+	fifo8_put(&mousefifo, data);
+	return;
 }
 
 void inthandler27(int *esp)
